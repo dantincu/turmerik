0f0522617b96eda39c482e7de4acab8f72e2a252
Daniel Tincu <tincuvd@gmail.com>
2022-07-18 13:11:59 +0300

    Made the microsoft identity authenticatio work

--- dotnet/Turmerik.OneDriveExplorer.AspNetCore.WebApi/Controllers/WeatherForecastController.cs
+++ dotnet/Turmerik.OneDriveExplorer.AspNetCore.WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Turmerik.OneDriveExplorer.AspNetCore.WebApi.Controllers
     [Authorize]
     [ApiController]
     [Route("[controller]")]
-    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    // [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class WeatherForecastController : ControllerBase
     {
         private readonly GraphServiceClient _graphServiceClient;
