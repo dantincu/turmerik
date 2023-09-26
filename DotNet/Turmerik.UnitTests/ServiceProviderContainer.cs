abc7e865252cf05023ec21cc5975b4b8b520f519
Daniel Tincu <tincuvd@gmail.com>
2023-09-26 15:57:52 +0300

    Initial commit

---
+++ DotNet/Turmerik.UnitTests/ServiceProviderContainer.cs
@@ -0,0 +1,27 @@
+﻿using Turmerik.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turmerik.Dependencies;
+
+namespace Turmerik.Testing
+{
+    public class ServiceProviderContainer : ServiceProviderContainerBase
+    {
+        private ServiceProviderContainer()
+        {
+        }
+
+        public static Lazy<ServiceProviderContainer> Instance { get; } = new Lazy<ServiceProviderContainer>(
+            () => new ServiceProviderContainer(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        protected override void RegisterServices(
+            IServiceCollection services)
+        {
+            TrmrkServices.RegisterAll(services);
+        }
+    }
+}
---
+++ DotNet/Turmerik/Helpers/FuncH.Sync.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turmerik
+{
+    public static partial class FuncH
+    {
+        public static async Task<TOut> WithSync<TIn, TOut>(
+            this Task<TIn> inTask,
+            Func<TIn, TOut> convertor) => convertor(await inTask);
+
+        public static async Task<TVal> ActWithSync<TVal>(
+            this Task<TVal> task,
+            Action<TVal> callback)
+        {
+            var val = await task;
+            callback(val);
+
+            return val;
+        }
+
+        public static async Task<bool> ActIfSync(
+            this Task<bool> conditionTask,
+            Action ifTrueAction = null,
+            Action ifFalseAction = null)
+        {
+            var condition = await conditionTask;
+
+            condition.ActIf(
+                ifTrueAction,
+                ifFalseAction);
+
+            return condition;
+        }
+
+        public static async Task<T> IfSync<T>(
+            this Task<bool> conditionTask,
+            Func<T> ifTrueAction = null,
+            Func<T> ifFalseAction = null)
+        {
+            var condition = await conditionTask;
+
+            var retVal = condition.If(
+                ifTrueAction,
+                ifFalseAction);
+
+            return retVal;
+        }
+
+        public static async Task<TOut> IfNotNullSync<TIn, TOut>(
+            this Task<TIn> task,
+            Func<TIn, TOut> convertor,
+            Func<TOut> defaultValueFactory = null)
+        {
+            var inVal = await task;
+            TOut outVal;
+
+            if (inVal != null)
+            {
+                outVal = convertor(inVal);
+            }
+            else if (defaultValueFactory != null)
+            {
+                outVal = defaultValueFactory();
+            }
+            else
+            {
+                outVal = default;
+            }
+
+            return outVal;
+        }
+
+        public static async Task<TVal> ActIfNotNullSync<TVal>(
+            this Task<TVal> task,
+            Action<TVal> callback,
+            Action nullCallback = null)
+        {
+            var inVal = await task;
+
+            if (inVal != null)
+            {
+                callback(inVal);
+            }
+            else if (nullCallback != null)
+            {
+                nullCallback();
+            }
+
+            return inVal;
+        }
+    }
+}
