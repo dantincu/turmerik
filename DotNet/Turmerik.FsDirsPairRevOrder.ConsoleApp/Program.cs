1501ea959a2c3927d985b055b77d9e325df4c567
dantincu <tincuvd@gmail.com>
2023-11-24 08:52:17 +0200

    Added FsDirsPairRevOrder console app

---
+++ DotNet/Turmerik.FsDirsPairRevOrder.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Turmerik.Dependencies;
+using Turmerik.DriveExplorer;
+using Turmerik.FsDirsPairRevOrder.ConsoleApp;
+using Turmerik.Helpers;
+
+var services = TrmrkServices.RegisterAll(
+    new ServiceCollection());
+
+services.AddSingleton<IDriveExplorerService, FsExplorerService>();
+
+services.AddTransient<ProgramComponent>();
+var svcProv = services.BuildServiceProvider();
+
+ConsoleH.TryExecute(
+    () =>
+    {
+        var program = svcProv.GetRequiredService<ProgramComponent>();
+        program.Run(args);
+    },
+    false);
