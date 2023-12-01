4a94aac0e7cd9e5eaad680b4677eea2c16364831
dantincu <tincuvd@gmail.com>
2023-12-01 16:44:37 +0200

    Completed the markdown table convertor

--- DotNet/Turmerik.Utility.WinFormsApp/Services/TextToMdService.cs
+++ DotNet/Turmerik.Utility.WinFormsApp/Services/TextToMdService.cs
@@ -4,15 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Turmerik.Core.Helpers;
+using Turmerik.Core.Text;
 
 namespace Turmerik.Utility.WinFormsApp.Services
 {
     public class TextToMdService
     {
+        public const string MD_TABLE_CELL_DELIM_STR = "|";
+        public const string MD_TABLE_HEADER_ROW_DELIM_CELL_STR = "--";
+
         public string SrcTextToMdTable(
-            SrcTextToMdTableOpts opts) => ConvertText(
-                opts.InputText,
-                (line, idx) => throw new NotImplementedException());
+            SrcTextToMdTableOpts opts)
+        {
+            string outputText;
+            string inputText = opts.InputText;
+            string separator = opts.Separator;
+            bool insertSpacesBetweenTokens = opts.InsertSpacesBetweenTokens;
+
+            if (!string.IsNullOrWhiteSpace(
+                inputText))
+            {
+                int? headerCellsCount = null;
+
+                outputText = ConvertText(
+                    inputText, (line, idx) =>
+                    {
+                        string outputText = SrcTextLineToMdTableLine(
+                            line, separator,
+                            out int cellsCount,
+                            insertSpacesBetweenTokens);
+
+                        headerCellsCount ??= cellsCount;
+                        return outputText;
+                    },
+                    null, outputLines =>
+                    {
+                        string mdTableCellDelimStr = GetMdTableCellDelimStr(
+                            insertSpacesBetweenTokens);
+
+                        if (opts.FirstLineIsHeader)
+                        {
+                            string headerDelimLine = Enumerable.Range(
+                                0, headerCellsCount!.Value).Select(
+                                idx => MD_TABLE_HEADER_ROW_DELIM_CELL_STR).ToArray().JoinStr(
+                                mdTableCellDelimStr);
+
+                            outputLines = [outputLines[0], headerDelimLine, ..outputLines[1..^0]];
+                        }
+
+                        if (opts.SurroundLineWithCellSep)
+                        {
+                            outputLines = outputLines.Select(
+                                line => mdTableCellDelimStr.Arr(
+                                    line, mdTableCellDelimStr).JoinStr()).ToArray();
+                        }
+
+                        return outputLines;
+                    });
+            }
+            else
+            {
+                outputText = string.Empty;
+            }
+
+            return outputText;
+        }
 
         public string ResultTextRmMdQtLvl(
             string inputText,
@@ -35,8 +91,8 @@ namespace Turmerik.Utility.WinFormsApp.Services
         private string ConvertText(
             string inputText,
             Func<string, int, string> lineConvertor,
-            Func<string[], string[]>? outputLinesTransformer = null,
-            Func<string[], string[]>? inputLinesTransformer = null)
+            Func<string[], string[]>? inputLinesTransformer = null,
+            Func<string[], string[]>? outputLinesTransformer = null)
         {
             string[] linesArr = inputText.Split('\n').With(
                 inputLinesTransformer.FirstNotNull(arr => arr));
@@ -51,13 +107,40 @@ namespace Turmerik.Utility.WinFormsApp.Services
 
         private string SrcTextLineToMdTableLine(
             string inputLine,
-            string sep,
-            out int cellsCount)
+            string separator,
+            out int cellsCount,
+            bool insertSpacesBetweenSymbols)
         {
-            var cellsArr = inputLine.Split(sep);
+            var cellsArr = inputLine.Split(separator);
             cellsCount = cellsArr.Length;
 
-            throw new NotImplementedException();
+            string mdTableCellDelimStr = GetMdTableCellDelimStr(
+                insertSpacesBetweenSymbols);
+
+            string outputLine = string.Join(
+                mdTableCellDelimStr,
+                cellsArr);
+
+            return outputLine;
         }
+
+        private string SrcTextLineToMdTableLine(
+            string inputLine,
+            string separator,
+            bool insertSpacesBetweenSymbols) => SrcTextLineToMdTableLine(
+                inputLine, separator, out _, insertSpacesBetweenSymbols);
+
+        private string GetMdTableCellDelimStr(
+            bool insertSpacesBetweenSymbols) => GetDelimStrCore(
+                MD_TABLE_CELL_DELIM_STR,
+                insertSpacesBetweenSymbols);
+
+        private string GetDelimStrCore(
+            string delimStr,
+            bool insertSpacesBetweenSymbols) => insertSpacesBetweenSymbols switch
+            {
+                true => $" {delimStr} ",
+                false => delimStr
+            };
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # turmerik
 Code for Turmerik Apps Suite - will contain cross platform apps like drive explorer apps (both for local device and for cloud storage providers - OneDrive, GoogleDrive, Dropbox), note taking apps, and other utility tools, including coding / refactoring helper tools
 
-asdf|asdf
---|--
-asdf|asdf
+asdf | asdf  
+ -- | --  
+asdf | asdf  
+ | 
 
->asdfasdf
->>asdf
