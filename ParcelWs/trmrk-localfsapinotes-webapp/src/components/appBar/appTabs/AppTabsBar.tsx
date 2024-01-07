2b9cc444e3b1fedcefffe9ccfe1c51769af26fcf
dantincu <tincuvd@gmail.com>
2024-01-07 16:57:16 +0200

    Added blur spacers for app tabs bar

--- ParcelWs/trmrk-localfsapinotes-webapp/src/components/appBar/appTabs/AppTabsBar.tsx
+++ ParcelWs/trmrk-localfsapinotes-webapp/src/components/appBar/appTabs/AppTabsBar.tsx
@@ -12,9 +12,11 @@ export default function AppPageBar() {
 
   return (<Box className="trmrk-app-tabs-bar" sx={{
     position: "absolute", left: "5em", right: "5em", overflow: "hidden", height: "2.5em", whiteSpace: "nowrap" }}>
+    <Box className="trmrk-tabs-list-spacer trmrk-before" />
     <Box className="trmrk-tabs-list" sx={{
         display: "inline-flex", position: "relative", height: "2.2em", top: "0.25em" }}>
       {openTabs.map(tab => <TabHead tab={tab} key={tab.tabUuid} />)}
     </Box>
+    <Box className="trmrk-tabs-list-spacer trmrk-after" />
   </Box>)
 }
