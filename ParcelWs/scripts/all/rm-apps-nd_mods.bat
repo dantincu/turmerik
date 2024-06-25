2a3d1663cd36ab03245a779e77c1ba0422e6b860
dantincu <tincuvd@gmail.com>
2024-06-25 09:33:37 +0300

    Updated packages

--- ParcelWs/scripts/all/rm-apps-nd_mods.bat
+++ ParcelWs/scripts/all/rm-apps-nd_mods.bat
@@ -1,7 +1,7 @@
 @echo off
 
-for %%a in ("..\..\apps\trmrk-notes-blazorapp",
-"..\..\apps\trmrk-react-testapp") do (
+for %%a in ("..\..\apps\trmrk-react-testapp",
+"..\..\apps\trmrk-notes-reactapp") do (
   cd %%a
   .\_\rm-nd_mods.bat
 )
--- ParcelWs/scripts/all/rm.bat
+++ ParcelWs/scripts/all/rm.bat
@@ -4,7 +4,6 @@ for %%a in ("..\..\libs\trmrk",
 "..\..\libs\trmrk-axios",
 "..\..\libs\trmrk-blazor",
 "..\..\libs\trmrk-browser",
-"..\..\libs\trmrk-browser-testapp",
 "..\..\libs\trmrk-react",
 "..\..\libs\trmrk-mkscripts-parcelws-sync-behavior",
 "..\..\libs\trmrk-mkscripts-parcelws-sync-behavior-testapp",
@@ -12,8 +11,8 @@ for %%a in ("..\..\libs\trmrk",
 "..\..\libs\trmrk-text-transform-behavior-testapp",
 "..\..\libs\trmrk-text-transform-defaultbehavior",
 "..\..\libs\trmrk-text-transform-mybehavior",
-"..\..\apps\trmrk-notes-blazorapp",
-"..\..\apps\trmrk-react-testapp") do (
+"..\..\apps\trmrk-react-testapp",
+"..\..\apps\trmrk-notes-reactapp") do (
   cd %%a
   .\_\rm.bat
 )
