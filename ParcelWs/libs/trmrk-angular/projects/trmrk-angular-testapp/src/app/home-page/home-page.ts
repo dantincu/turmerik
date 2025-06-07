75e2021c5ec6adb64510acd974d2cf8a356981e4
dantincu <tincuvd@gmail.com>
2025-06-07 16:40:12 +0300

    Added app icon

--- ParcelWs/libs/trmrk-angular/projects/trmrk-angular-testapp/src/app/home-page/home-page.ts
+++ ParcelWs/libs/trmrk-angular/projects/trmrk-angular-testapp/src/app/home-page/home-page.ts
@@ -3,9 +3,17 @@ import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 
+import { TrmrkAppIcon } from '../trmrk-app-icon/trmrk-app-icon';
+
 @Component({
   selector: 'app-home-page',
-  imports: [MatIconModule, MatButtonModule, MatIconButton, RouterLink],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatIconButton,
+    RouterLink,
+    TrmrkAppIcon,
+  ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
---
+++ ParcelWs/libs/trmrk-angular/projects/trmrk-angular-testapp/src/app/trmrk-app-icon/trmrk-app-icon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trmrk-app-icon',
+  imports: [],
+  templateUrl: './trmrk-app-icon.html',
+  styleUrl: './trmrk-app-icon.scss'
+})
+export class TrmrkAppIcon {
+
+}
