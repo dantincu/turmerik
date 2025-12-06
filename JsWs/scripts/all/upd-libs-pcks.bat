bea03dff736cc4453982206d61ef722b6e2688bc
dantincu <tincuvd@gmail.com>
2025-12-06 04:51:17 +0200

    +

--- JsWs/scripts/all/upd-libs-pcks.bat
+++ JsWs/scripts/all/upd-libs-pcks.bat
@@ -1,20 +1,19 @@
 @echo off
 
 for %%a in ("..\..\libs\trmrk",
-"..\..\libs\trmrk-angular\projects\trmrk-angular",
+"..\..\libs\trmrk-angular",
 "..\..\libs\trmrk-axios",
 "..\..\libs\trmrk-browser",
-"..\..\libs\trmrk-filemanager-nglib\projects\trmrk-filemanager-nglib",
-"..\..\libs\trmrk-mailbox-nglib\projects\trmrk-mailbox-nglib",
+"..\..\libs\trmrk-filemanager-nglib",
+"..\..\libs\trmrk-mailbox-nglib",
 "..\..\libs\trmrk-mkscripts-jsws-sync-behavior",
 "..\..\libs\trmrk-mkscripts-jsws-sync-behavior-testapp",
-"..\..\libs\trmrk-notes-nglib\projects\trmrk-notes-nglib",
-"..\..\libs\trmrk-testing-nglib\projects\trmrk-testing-nglib",
+"..\..\libs\trmrk-notes-nglib",
+"..\..\libs\trmrk-testing-nglib",
 "..\..\libs\trmrk-text-transform-behavior",
 "..\..\libs\trmrk-text-transform-behavior-testapp",
 "..\..\libs\trmrk-text-transform-defaultbehavior",
-"..\..\libs\trmrk-text-transform-mybehavior",
-"..\..\libs\trmrk-angular\projects\trmrk-angular-testapp") do (
+"..\..\libs\trmrk-text-transform-mybehavior") do (
   cd %%a
   .\_\upd-pcks.bat
 )
