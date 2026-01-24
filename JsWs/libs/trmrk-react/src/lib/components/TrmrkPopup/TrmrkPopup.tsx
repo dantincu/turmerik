cb141466dcb7d667c346cab26adfc78e3ea5309d
dantincu <tincuvd@gmail.com>
2026-01-24 12:32:27 +0200

    +

--- JsWs/libs/trmrk-react/src/lib/components/TrmrkPopup/TrmrkPopup.tsx
+++ JsWs/libs/trmrk-react/src/lib/components/TrmrkPopup/TrmrkPopup.tsx
@@ -132,7 +132,7 @@ export default function TrmrkPopup(
         }
       }
     }
-  }, [show, messageFadeOut, showEl, autoCloseEl])
+  }, [show, autoCloseEl])
 
   React.useEffect(() => {
     return () => {
