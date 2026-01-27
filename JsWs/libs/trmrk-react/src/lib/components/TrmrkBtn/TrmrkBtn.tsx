cafa5ab28f9d400b2c54a74ae3a3df54e0e2c687
dantincu <tincuvd@gmail.com>
2026-01-27 07:32:49 +0200

    +

--- JsWs/libs/trmrk-react/src/lib/components/TrmrkBtn/TrmrkBtn.tsx
+++ JsWs/libs/trmrk-react/src/lib/components/TrmrkBtn/TrmrkBtn.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 import "./TrmrkBtn.scss";
 
-import { NullOrUndef } from "@/src/trmrk/core";
+import { NullOrUndef, actWithValIf } from "@/src/trmrk/core";
 
-import { effectCallback, handleOnPointerDownFunc, refElAvailableFunc } from "./TrmrkBtnService";
+import { clearRefVal, updateRef } from "../../services/utils";
 import { ComponentProps } from "../defs/common";
 
 export interface TrmrkBtnProps extends React.ComponentPropsWithRef<'button'>, ComponentProps {
@@ -16,18 +16,34 @@ export interface TrmrkBtnProps extends React.ComponentPropsWithRef<'button'>, Co
 const TrmrkBtn = React.memo(React.forwardRef<HTMLButtonElement, TrmrkBtnProps>(({ className, children, borderWidth, onPointerDown, ...props }, ref) => {
   const rootElRef = React.useRef<HTMLButtonElement | null>(null);
   const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+  const [ showIsPressedCssClass, setShowIsPressedCssClass ] = React.useState(false);
 
-  const handleOnPointerDown = React.useCallback(
-    (e: React.PointerEvent<HTMLButtonElement>) => handleOnPointerDownFunc(e, timeoutRef, onPointerDown), []);
+  const handleOnPointerDown = React.useCallback((e: React.PointerEvent<HTMLButtonElement>) => {
+    setShowIsPressedCssClass(true);
+  
+    timeoutRef.current = setTimeout(() => {
+      setShowIsPressedCssClass(false);
+      timeoutRef.current = null;
+    }, 200);
+  
+    actWithValIf(onPointerDown, (f) => f(e));
+  }, []);
 
-  const refElAvailable = React.useCallback(
-    (el: HTMLButtonElement | null) => refElAvailableFunc(el, rootElRef, ref), []);
+  const refElAvailable = React.useCallback((el: HTMLButtonElement | null) => {
+    rootElRef.current = el;
+    actWithValIf(ref, (r) => updateRef(r, el));
+  }, []);
 
-  React.useEffect(() => effectCallback(rootElRef, timeoutRef), []);
+  React.useEffect(() => {
+    return () => {
+      clearRefVal(timeoutRef, clearTimeout);
+    };
+  }, []);
 
   return (
     <button {...props} ref={refElAvailable} className={[
         'trmrk-btn',
+        showIsPressedCssClass ? 'trmrk-btn-pressed' : '',
         ((borderWidth ?? null) !== null ? `trmrk-border trmrk-border-${borderWidth}px` : ''),
         className ?? ''
       ].join(' ')}
