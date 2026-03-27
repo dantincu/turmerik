a590fe7b28097e723f7d798820646a380082fc11
dantincu <tincuvd@gmail.com>
2026-03-27 14:38:48 +0200

    +

--- JsWs/libs/trmrk-react/src/lib/components/TrmrkItemsList/TrmrkItemsList.tsx
+++ JsWs/libs/trmrk-react/src/lib/components/TrmrkItemsList/TrmrkItemsList.tsx
@@ -5,15 +5,13 @@ import { NullOrUndef } from "@/src/trmrk/core";
 
 export interface TrmrkItemsListProps extends React.ComponentPropsWithRef<'div'> {
   tagName?: React.ElementType | NullOrUndef;
-  itemsAreOutlined?: boolean | NullOrUndef;
 }
 
-const TrmrkItemsList = React.forwardRef(({children, className, tagName, itemsAreOutlined, ...props}: TrmrkItemsListProps, ref: React.ForwardedRef<HTMLDivElement>) => {
+const TrmrkItemsList = React.forwardRef(({children, className, tagName, ...props}: TrmrkItemsListProps, ref: React.ForwardedRef<HTMLDivElement>) => {
   const List = tagName ?? 'div';
   
   return <List className={[
         "trmrk-items-list",
-        itemsAreOutlined ? "trmrk-items-are-outlined" : "",
         className ?? ""]} {...props} ref={ref as React.RefObject<HTMLDivElement>}>
     {children}
     </List>
