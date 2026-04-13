55e11bbf439c19ea2256c66e736e09cbdac5d6e1
dantincu <tincuvd@gmail.com>
2026-04-13 11:19:40 +0300

    +

---
+++ Scripts/Deploy/Utility-Apps/cp-ayran-dotnet-util-bins.bat
@@ -0,0 +1,1 @@
+_bckp :pf:cp-ayran-dotnet-util-bins
---
+++ Scripts/global/_cvdc.bat
@@ -0,0 +1,2 @@
+@echo off
+"%USERPROFILE%\AppData\Roaming\Ayran\Apps\Bin\docs-converter\ayran-docs-converter-cmd\AyranDocsConverter\Release\net10.0\AyranDocsConverter.exe" %*
